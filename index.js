d383e5b39f8e3fa3c3e020d10aa14c1b5f4f0970
WDPronovost <44554138+wdpronovost@users.noreply.github.com>
2022-09-18 16:19:24 +0000

    build: completed project

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  res.json({"name":req.file.originalname,"type":req.file.mimetype,"size":req.file.size})
+})
 
 
 
